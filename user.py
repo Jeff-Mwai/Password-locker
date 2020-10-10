c048c86134b737688d6967c7259d63a593644709
Jeff-Mwai <jeffmwai3@gmail.com>
2020-10-10 12:36:16 +0300

    Created main and users class

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-users = []
+users = list()
 
 class Users:
 
@@ -6,21 +6,30 @@ class Users:
 
         self.username = username
         self.password = password
-
-
-
-
+        
+    def login():
+        print("Put your credentials to login")
+        username = input("Username: ")
+        password = input("Password: ")
+        user1 = users(username, password)
+        if username ==user1.username and password == user1.password:
+            print("Login successful")
+        else:
+            print("Error")
 
 class Main:
 
-    def __init__(self, username,password):
-
-        self.username = username
-        self.password = password
-
     def create_account():
         user1 = Users(input("Username: "), input("Password: "))
+        users.append(user1)
+        Users.login()
         
+        for x in users:
+            print(x.username)
+        
+
+   
+
 
 
     print("Hello, welcome to Password Locker. Choose one of the three options to continue:")
@@ -28,7 +37,7 @@ class Main:
     user_option = input()
 
     if user_option == "1":
-        print("Login")
+        login()
     elif user_option == "2":
         create_account()
     elif user_option == "3":
