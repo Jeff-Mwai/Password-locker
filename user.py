b77b1fb8dc0ec05324a08578624a0e11ee006989
Jeff-Mwai <jeffmwai3@gmail.com>
2020-10-12 14:23:09 +0300

    Added various docstrings to the file

--- user.py
+++ user.py
@@ -5,12 +5,21 @@ import string
 
 users = list()
 credentials = list()
+
+"""
+declared two empty lists to store data
+"""
+
 class Credentials:
     
     def __init__(self, account_name, username, password):
         self.account_name = account_name
         self.username = username
         self.password = password
+
+        """
+        Created the class credentials and initialized using the self keyword
+        """
 
     def my_credentials():
         print("Choose one of the following options to continue")
@@ -60,6 +69,10 @@ class Users:
 
         self.username = username
         self.password = password
+
+        """
+        Created class users and initialized using the self keyword
+        """
         
     def login():
         print("Put your credentials to login")
